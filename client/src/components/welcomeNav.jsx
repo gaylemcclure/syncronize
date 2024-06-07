c14911f181fa69770b456aaacdd8207531684f1a
gaylemcclure <gaylekdennison@gmail.com>
2024-06-07 16:44:19 +1000

    readding client folders

---
+++ client/src/components/welcomeNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+
+const WelcomeNav = () => {
+  return (
+    <div>
+    <h1>Logo</h1>
+    <h2>Services</h2>
+    <h2>Products</h2>
+    <button>Sign up</button>
+    </div>
+  )
+}
+
+export default WelcomeNav;
---
+++ client/src/pages/welcomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import WelcomeNav from '../components/welcomeNav';
+
+const WelcomePage = () => {
+
+  return (
+    <div>
+    <WelcomeNav />
+    <h1>Welcome page THJDKJNKJNKJNKJ</h1>
+    </div>
+  )
+
+}
+
+export default WelcomePage;
